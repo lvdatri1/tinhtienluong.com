81acb6e384022b155a91d0862df15be683717a76
Trinh Le <lvdatri@gmail.com>
2026-03-28 11:50:01 +1300

    fix: adsense verification script and ads.txt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
           async 
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5070036872443533"
           crossOrigin="anonymous"
-          strategy="afterInteractive"
+          strategy="beforeInteractive"
         />
       </head>
       <body>
